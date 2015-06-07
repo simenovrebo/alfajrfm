2a16920f885d2e4b6586fa8275f9e5cda1ad0396
Simen Øvrebø <simen@simenovrebo.no>
2015-06-07 05:38:12 +0200

    updates

--- iOS/Alfajr FMTests/Alfajr_FMTests.h
+++ iOS/Alfajr FMTests/Alfajr_FMTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Alfajr FM. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface Alfajr_FMTests : SenTestCase
+@interface Alfajr_FMTests : XCTestCase
 
 @end
--- iOS/Alfajr FMTests/Alfajr_FMTests.m
+++ iOS/Alfajr FMTests/Alfajr_FMTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in Alfajr FMTests");
+    XCTFail(@"Unit tests are not implemented yet in Alfajr FMTests");
 }
 
 @end
